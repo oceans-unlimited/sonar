442474aba106dee18ac9d2424d2daf0cad9a92db
Seth Gorham <seth@sethgorham.com>
2026-04-24 23:30:00 -0400

    server listen in vite config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
 
     server: {
         port: 5173,
+        host: '0.0.0.0',
         // Proxy Socket.IO requests to the Express server
         proxy: {
             '/socket.io': {
